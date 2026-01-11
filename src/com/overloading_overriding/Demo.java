5d8cc41159582e5ab8825725a9b6bc9db8ecd135
Aditya Deshmukh <adityadeshmukh14100@gmail.com>
2026-01-11 08:20:02 -0800

    Added core java concepts

---
+++ src/com/overloading_overriding/Demo.java
@@ -0,0 +1,13 @@
+package com.overloading_overriding;
+
+import com.access_modifiers.Main;
+
+public class Demo extends Main{
+	public static void main(String[] args) {
+		
+		Demo demo = new Demo();
+		System.out.println(demo.a);
+		System.out.println(demo.c);
+		
+	}
+}
