eb19f6d34de9739ad41eae3495a382eb1c0f5f45
Aditya Deshmukh <adityadeshmukh14100@gmail.com>
2026-01-31 07:52:10 -0800

    Added core java concepts

--- src/MultiThreading/ThreadCreationDemo.java
+++ src/MultiThreading/ThreadCreationDemo.java
@@ -1,11 +1,23 @@
 package MultiThreading;
 
+/*
+ * Description :
+ * This program demonstrates three different ways to create and start
+ * threads in Java using the Thread class and Runnable interface.
+ *
+ * 1) By extending the Thread class and overriding the run() method.
+ * 2) By creating an anonymous class that extends Thread.
+ * 3) By implementing the Runnable interface using an anonymous class.
+ *
+ */
+
+
 public class ThreadCreationDemo extends Thread{
 	
 	//1st method!
 	@Override
 	public void run() {
-		System.out.println("thread1 is running...");    
+		System.out.println("thread1 is running....");    
 	}
 
 	public static void main(String[] args) {
@@ -26,8 +38,15 @@ public class ThreadCreationDemo extends Thread{
 		new Thread(new Runnable() {
 			@Override
 			public void run() {
-				System.out.println("thread3 is running.....");
+				System.out.println("thread3 is running....");
 			}
 		}).start(); 
 	}
 }
+
+/*
+Output:
+thread1 is running....
+thread2 is running....
+thread3 is running....
+*/
