d6338c547a45fb09cd75d73ae2f6deaec2b80beb
Aditya Deshmukh <adityadeshmukh14100@gmail.com>
2026-01-31 08:02:52 -0800

    Added core java concepts

--- src/ThreadCommunication/InterThreadCommunication.java
+++ src/ThreadCommunication/InterThreadCommunication.java
@@ -1,5 +1,28 @@
 package ThreadCommunication;
 
+/* Description :
+ * This program demonstrates Inter-Thread Communication in Java
+ * using the wait() and notify() methods.
+ *
+ * A shared Message object is used as a communication medium
+ * between two threads:
+ *
+ * 1) Waiting Thread:
+ *    - Acquires the lock on the shared object
+ *    - Calls wait() and releases the lock
+ *    - Resumes execution after receiving notification
+ *
+ * 2) Notifying Thread:
+ *    - Sleeps for 2 seconds
+ *    - Acquires the same lock
+ *    - Calls notify() to wake up the waiting thread
+ *
+ * Important Rules:
+ * - wait() and notify() must be called inside a synchronized block
+ * - Both threads must use the same shared object
+ *
+ */
+
 class Message{
 	String msg;
 }
@@ -7,7 +30,7 @@ class Message{
 public class InterThreadCommunication {
 	public static void main(String[] args) {
 		
-		Message message  = new Message();
+		Message message  = new Message();  // Shared object
 		
 		new Thread(new Runnable() {
 			@Override
@@ -15,7 +38,7 @@ public class InterThreadCommunication {
 				synchronized (message) {
 					System.out.println("waiting for msg....");
 					try {
-						message.wait();
+						message.wait();  // Thread goes into waiting state
 					} catch (InterruptedException e) {
 						e.printStackTrace();
 					}
@@ -29,16 +52,26 @@ public class InterThreadCommunication {
 			@Override
 			public void run() {
 				try {
-					Thread.sleep(2000);
+					Thread.sleep(2000);  // Delay before sending notification
 				} catch (InterruptedException e) {
 					e.printStackTrace();
 				}
 				synchronized (message) {
 					System.out.println("sending the msg....");
-					message.notify();
+					message.notify();  // Wakes up waiting thread
 				}
 			}
 		}).start();
 		
 	}
 }
+
+
+/*
+
+waiting for msg....
+--- waits 2 sec----
+sending the msg....
+got the msg!
+
+*/
